b4e7fbf1714334e4066b80809c0834067496e434
odilon-cloud <oirambna@gmail.com>
2022-07-26 22:04:27 +0000

    commit

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # a Ruby script that accepts one argument and pass it to a regular expression matching method
-# The regular expression must match Holberton
-puts ARGV[0].scan(/Holberton/).join
+# The regular expression must match School
+puts ARGV[0].scan(/School/).join
